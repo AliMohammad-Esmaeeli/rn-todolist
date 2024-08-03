3f2782c40711e1cf445aa0df7df71e1dbe300856
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2024-08-03 15:12:11 +0330

    feat: Add TodoItem & List that

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,9 @@ import { useState } from 'react';
 
 import AppBar from '@/components/Todo/AppBar';
 import TodoType from '@/components/Todo/TodoType';
-
+import Order from '@/components/Todo/Order';
+import TodoItem from '@/components/Todo/TodoItem';
+import NewTodoNotFound from '@/components/Todo/NewTodoNotFound';
 
 
 
@@ -15,6 +17,23 @@ export default function HomeScreen() {
     <View className="flex-1 items-center justify-between bg-[#b9ffff] h-screen w-screen relative">
       {/* AppBar */}
       <AppBar />
+
+      {/* Todo List */}
+      <View className='flex justify-center items-center flex-col h-[85vh]'>
+        {
+          works.length != 0 ?
+            <>
+              <Order works={works} setWorks={setWorks} />
+              <FlatList
+                data={works}
+                renderItem={(item) => <TodoItem item={item} works={works} setWorks={setWorks} />
+                }
+                keyExtractor={item => item.id}
+              />
+            </> :
+            <NewTodoNotFound />
+        }
+      </View>
     </View>
   );
 }
---
+++ components/Todo/TodoItem.tsx
@@ -0,0 +1,56 @@
+import { Image, ListRenderItemInfo, Pressable, Text, View } from "react-native";
+import Feather from '@expo/vector-icons/Feather';
+import { Dispatch, SetStateAction } from 'react';
+import TodoType from './TodoType';
+
+interface PropsType {
+    item: ListRenderItemInfo<TodoType>,
+    setWorks: Dispatch<SetStateAction<TodoType[]>>,
+    works: TodoType[],
+}
+
+export default function TodoItem(props: PropsType) {
+    return (
+        <View className={`flex justify-around items-center flex-row gap-10 my-3 rounded-3xl w-96 h-32 pb-6 ${props.item.item.color}`}>
+            <View className='flex justify-center items-center flex-col gap-2'>
+                <Pressable onPress={() => {
+                    props.setWorks(
+                        props.works.map(item => item.id === props.item.item.id ? {
+                            id: props.item.item.id,
+                            title: props.item.item.title,
+                            description: props.item.item.description,
+                            done: props.item.item.done ? false : true,
+                            time: props.item.item.time,
+                            color: props.item.item.color,
+                            priority: props.item.item.priority,
+                        } : item)
+                    )
+                }}>
+                    {
+                        props.item.item.done ?
+                            <Image className='h-12 w-12' source={require("@/assets/images/icons/check.png")} /> :
+                            <Image className='h-10 w-10' source={require("@/assets/images/icons/square.png")} />
+                    }
+                </Pressable>
+                <Pressable onPress={() => {
+                    props.setWorks((pervWorks) => {
+                        return pervWorks.filter((work) => work.id != props.item.item.id)
+                    })
+                }}>
+                    <Feather name="trash-2" size={38} color="#2145b7" />
+                </Pressable>
+            </View>
+            <View className='flex justify-center items-center gap-1'>
+                <Text style={{ fontFamily: "YekanBakh" }} className='text-xl text-[#2145b7]'>
+                    {props.item.item.title}
+                </Text>
+                <Text style={{ fontFamily: "YekanBakh" }} className='text-lg text-[#2145b7]'>
+                    {props.item.item.description}
+                </Text>
+                <Text style={{ fontFamily: "YekanBakh" }} className='text-base text-[#2145b7]'>
+                    {props.item.item.priority}
+                </Text>
+            </View>
+        </View>
+    )
+}
