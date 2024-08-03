9723f364183dbed29bcbb280209272512b57f650
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2024-08-03 15:13:40 +0330

    feat: Add order component

---
+++ components/Todo/Order.tsx
@@ -0,0 +1,31 @@
+import { Pressable, Text, View } from "react-native";
+import { Dispatch, SetStateAction } from "react";
+import TodoType from "./TodoType";
+
+interface PropsType {
+    works: TodoType[],
+    setWorks: Dispatch<SetStateAction<TodoType[]>>
+}
+
+export default function Order(props: PropsType) {
+    return (
+        <View className="flex justify-center items-center gap-3 flex-row w-screen my-4">
+            <View className="flex justify-center items-center h-10 w-28 border-2 border-white rounded-lg">
+                <Pressable onPress={() => props.setWorks([...props.works].sort())}>
+                    <Text style={{ fontFamily: "YekanBakh" }} className="text-black text-sm">بر اساس الفبا</Text>
+                </Pressable>
+            </View>
+            <View className="flex justify-center items-center h-10 w-28 border-2 border-white rounded-lg">
+                <Pressable onPress={() => props.setWorks([...props.works].sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime())
+                )}>
+                    <Text style={{ fontFamily: "YekanBakh" }} className="text-black text-sm">بر اساس زمان</Text>
+                </Pressable>
+            </View>
+            <View className="flex justify-center items-center h-10 w-28 border-2 border-white rounded-lg">
+                <Pressable onPress={() => props.setWorks([...props.works].sort())}>
+                    <Text style={{ fontFamily: "YekanBakh" }} className="text-black text-sm">براساس اولویت</Text>
+                </Pressable>
+            </View>
+        </View>
+    )
+}
