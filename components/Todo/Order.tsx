8805400a391cdddbb7606ad4fc9bd0eaa6ca30af
Ali_Mohammad_Esmaeeli <alimohammadesmaeeli4456@gmail.com>
2024-08-04 16:13:42 +0330

    fix: Now you can order by alphabet, time and priority

--- components/Todo/Order.tsx
+++ components/Todo/Order.tsx
@@ -11,18 +11,56 @@ export default function Order(props: PropsType) {
     return (
         <View className="flex justify-center items-center gap-3 flex-row w-screen my-4">
             <View className="flex justify-center items-center h-10 w-28 border-2 border-white rounded-lg">
-                <TouchableOpacity onPress={() => props.setWorks([...props.works].sort())}>
+                <TouchableOpacity onPress={() => props.setWorks([...props.works].sort((a, b) => {
+                    return a.title.localeCompare(b.title)
+                }))}>
                     <Text style={{ fontFamily: "YekanBakh" }} className="text-black text-sm">بر اساس الفبا</Text>
                 </TouchableOpacity>
             </View>
             <View className="flex justify-center items-center h-10 w-28 border-2 border-white rounded-lg">
-                <TouchableOpacity onPress={() => props.setWorks([...props.works].sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime())
+                <TouchableOpacity onPress={() => props.setWorks([...props.works].sort((a, b) => {
+                    return new Date(a.time).getTime() - new Date(b.time).getTime()
+                })
                 )}>
                     <Text style={{ fontFamily: "YekanBakh" }} className="text-black text-sm">بر اساس زمان</Text>
                 </TouchableOpacity>
             </View>
             <View className="flex justify-center items-center h-10 w-28 border-2 border-white rounded-lg">
-                <TouchableOpacity onPress={() => props.setWorks([...props.works].sort())}>
+                <TouchableOpacity onPress={() => props.setWorks([...props.works].sort((a, b) => {
+                    let c;
+                    let d;
+                    switch (a.priority) {
+                        case "پایین":
+                            c = 4;
+                            break;
+                        case "متوسط":
+                            c = 3
+                            break;
+                        case "بالا":
+                            c = 2
+                            break;
+                        case "اضطراری":
+                            c = 1
+                            break;
+                    }
+                    switch (b.priority) {
+                        case "پایین":
+                            d = 4;
+                            break;
+                        case "متوسط":
+                            d = 3
+                            break;
+                        case "بالا":
+                            d = 2
+                            break;
+                        case "اضطراری":
+                            d = 1
+                            break;
+
+                    }
+                    return c! - d!
+                }
+                ))}>
                     <Text style={{ fontFamily: "YekanBakh" }} className="text-black text-sm">براساس اولویت</Text>
                 </TouchableOpacity>
             </View>
